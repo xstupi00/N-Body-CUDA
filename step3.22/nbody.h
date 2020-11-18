a2ba301142b56c9af5b53a613c7d7a1e189fdef1
dd-20-28-245 <dd-20-28-245@cn199.bullx>
2020-11-18 23:03:20 +0100

    Add warp synchronous reduction

---
+++ step3.22/nbody.h
@@ -0,0 +1,77 @@
+/**
+ * @File nbody.h
+ *
+ * Header file of your implementation to the N-Body problem
+ *
+ * Parallel Computations on GPU (PCG 2020)
+ * Assignment no. 1 (cuda)
+ * Login: xstupi00
+ */
+
+#ifndef __NBODY_H__
+#define __NBODY_H__
+
+#include <cstdlib>
+#include <cstdio>
+#include "h5Helper.h"
+
+/* Gravitation constant */
+constexpr float G =  6.67384e-11f;
+constexpr float COLLISION_DISTANCE = 0.01f;
+
+
+/**
+ * Particles data structure
+ */
+typedef struct
+{
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //                 FILL IN: Particle data structure optimal for the use on GPU (step 0)                             //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  float* pos_x;
+  float* pos_y;
+  float* pos_z;
+  float* vel_x;
+  float* vel_y;
+  float* vel_z;
+  float* weight;
+} t_particles;
+
+/**
+ * CUDA kernel to calculate gravitation and collision velocity and update particles.
+ * @param p_in    - input particles
+ * @param p_out   - output particles
+ * @param N       - Number of particles
+ * @param dt      - Size of the time step
+ */
+__global__ void calculate_velocity(t_particles p, int N, float dt);
+
+/**
+ * CUDA kernel to update particles
+ * @param p       - particles
+ * @param comX    - pointer to a center of mass position in X
+ * @param comY    - pointer to a center of mass position in Y
+ * @param comZ    - pointer to a center of mass position in Z
+ * @param comW    - pointer to a center of mass weight
+ * @param lock    - pointer to a user-implemented lock
+ * @param N       - Number of particles
+ */
+__global__ void centerOfMass(t_particles p, float* comX, float* comY, float* comZ, float* comW, int* lock, const int N);
+
+__inline__ __device__ float4 block_reduce_sum(float4 partial_sums);
+
+__inline__ __device__ float4 warp_reduce_sum(float4 partial_sums);
+
+/**
+ * CPU implementation of the Center of Mass calculation
+ * @param memDesc - Memory descriptor of particle data on CPU side
+ */
+float4 centerOfMassCPU(MemDesc& memDesc);
+
+/**
+ * Check whether the given numner is the power of number two or it is not.
+ * @param x - Number to check whether it is the power of two
+ */
+bool ispow2(int x);
+
+#endif /* __NBODY_H__ */
